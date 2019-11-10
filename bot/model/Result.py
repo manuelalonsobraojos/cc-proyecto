fb1e6e441d5d3c182e6cbc1421a347882964a849
manuel <manuelalonso136@gmail.com>
2019-11-11 00:23:44 +0100

    Creando recopilacion de datos y servicio
    closes #6

---
+++ bot/model/Result.py
@@ -0,0 +1,38 @@
+from peewee import *
+from bot.model.BaseModel import BaseModel
+
+
+class Clasification(BaseModel):
+    id = AutoField(primary_key=True)
+    teamLocal = CharField(max_length=50, null=False)
+    teamVisit = CharField(max_length=50, null=False)
+    resultLocal = IntegerField(null=True)
+    resultVisit = IntegerField(null=True)
+
+    def getId(self):
+        return self.id
+
+    def getTeamLocal(self):
+        return self.teamLocal
+
+    def setTeamLocal(self, teamLocal):
+        self.teamLocal = teamLocal
+
+    def getTeamVisit(self):
+        return self.teamVisit
+
+    def setTeamVisit(self, teamVisit):
+        self.teamVisit = teamVisit
+
+    def getResultLocal(self):
+        return self.resultLocal
+
+    def setResultLocal(self, resultLocal):
+        self.resultLocal = resultLocal
+
+    def getResultVisit(self):
+        return self.resultVisit
+
+    def setResultVisit(self, resultVisit):
+        self.resultVisit = resultVisit
+
---
+++ bot/service/ClasificationService.py
@@ -0,0 +1,103 @@
+from bot.model.Result import Result
+import requests
+from bs4 import BeautifulSoup
+
+class ClasificationService():
+
+    @staticmethod
+    def addClasification():
+        try:
+            result = Result()
+            result.setTeamLocal("Granada")
+            result.setTeamVisit("Barcelona")
+            result.setResultLocal(2)
+            result.setResultVisit(1)
+            result.save()
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def getResult(id):
+
+        try:
+            result = Result.get(Result.id == id)
+        except:
+            return None
+        return result
+
+    @staticmethod
+    def inserDataResult():
+        """Funcion que scrapea de la web la información de los resultados de la jornada de la liga española de fútbol.
+        """
+        url = "http://resultados.as.com/resultados/futbol/primera/jornada/"
+
+        req = requests.get(url)
+
+        v_tlocal = []  # vector equipos locales
+        v_tvisit = []  # vector equipos visitantes
+        v_result = []  # vector resultados
+        v_result_local = []
+        v_result_visit = []
+        v_fecha = []  # vecto fechas
+
+        statusCode = req.status_code
+        if statusCode == 200:
+
+            # Pasamos el contenido HTML de la web a un objeto BeautifulSoup()
+            html = BeautifulSoup(req.text, "html.parser")
+
+            # Obtenemos todos los divs donde estan las entradas de los equipos locales
+            entradas = html.find_all('div', {'class': 'equipo-local'})
+            for i, entrada in enumerate(entradas):
+                equipo_local = entrada.find('span', {'class': 'nombre-equipo'}).getText()
+                v_tlocal.insert(i, equipo_local)
+
+            # Obtenemos todos los divs donde estan las entradas de los equipos visitantes
+            entradas2 = html.find_all('div', {'class': 'equipo-visitante'})
+            for i, entrada in enumerate(entradas2):
+                equipo_visit = entrada.find('span', {'class': 'nombre-equipo'}).getText()
+                v_tvisit.insert(i, equipo_visit)
+
+            # Obtenemos todos los divs donde estan los resultados finalizados
+            entradas3 = html.find_all('a', {'class': 'resultado'})
+            for i, entrada in enumerate(entradas3):
+                # resultado = entrada.find('a', {'class' : 'resultado'}).getText()
+                v_result.insert(i, entrada.text)
+                print(entrada.text)
+                result = entrada.text
+                position = result.find('-')
+                v_result_local.insert(i, int(""+result[position-2]))
+                v_result_visit.insert(i, int(""+result[position + 2]))
+
+            if len(v_result) < 10:
+                for i in range(len(v_result), 10):
+                    v_result.insert(i, "-")
+
+            # Obtenemos todos los divs donde estan las fechas
+            entradas4 = html.find_all('span', {'class': 'fecha'})
+            for i, entrada in enumerate(entradas4):
+                # resultado = entrada.find('a', {'class' : 'resultado'}).getText()
+                v_fecha.insert(i, entrada.text)
+
+            # print (entradas)
+
+            for i in range(10):
+                # fun_bd.insertResultados(v_tlocal[i], v_result[i], v_tvisit[i], i + 1)
+                try:
+                    result = Result.select().where(Result.id == i).get()
+                    result.setTeamLocal(v_tlocal[i])
+                    result.setTeamVisit(v_tvisit[i])
+                    result.setResultLocal(int(v_result_local[i]))
+                    result.setResultVisit(int(v_result_visit[i]))
+                    result.save()
+                except:
+                    result = Result()
+                    result.setTeamLocal(v_tlocal[i])
+                    result.setTeamVisit(v_tvisit[i])
+                    result.setResultLocal(int(v_result_local[i]))
+                    result.setResultVisit(int(v_result_visit[i]))
+                    result.save()
+
+        else:
+            print ("Status Code %d" % statusCode)
