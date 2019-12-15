a3ae2bd0d3de133dea093f8ca319b97632a8f0a9
manuel <manuelalonso136@gmail.com>
2019-12-15 08:16:53 -0800

    Actualización de dockerfile
    closes #44

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
 FROM ubuntu:16.04
 RUN apt-get -y update
-RUN apt-get install -y git
-COPY . /home/cc-proyecto/
-RUN cd /home/cc-proyecto && chmod a+x docker_run
-RUN cd /home/cc-proyecto && ./docker_run
-CMD cd /home/cc-proyecto && cd bot && python3 app.py
+
+#Creación de directorio de trabajo
+RUN mkdir -p /home/cc-proyecto
+
+#Establecimiento de directorio de trabajo
+WORKDIR /home/cc-proyecto
+
+#Copia de archivos necesarios al directorio de trabajo
+COPY docker_run Makefile setup.py bot/* ./
+
+#Permiso de Script de dependencias
+RUN chmod a+x docker_run
+
+#Ejecución de script de dependencias
+RUN ./docker_run
+
+#Ejecución de microservicio
+CMD python3 bot/app.py
--- bot/test/test_result.py
+++ bot/test/test_result.py
@@ -6,17 +6,17 @@ db.init(os.environ['NAME_BD'], host=os.environ['HOST_BD'], user=os.environ['USER
 class ResultTest(unittest.TestCase):
 
     def test_result_team_local(self):
-        result = Result.create(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
+        result = Result(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
         self.assertEqual(result.getTeamLocal(), "Granada")
 
     def test_result_team_visit(self):
-        result = Result.create(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
+        result = Result(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
         self.assertEqual(result.getTeamVisit(), "Valladolid")
 
     def test_result_local(self):
-        result = Result.create(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
+        result = Result(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
         self.assertEqual(result.getResultLocal(), 1)
 
     def test_result_visit(self):
-        result = Result.create(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
+        result = Result(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
         self.assertEqual(result.getResultVisit(), 1)
