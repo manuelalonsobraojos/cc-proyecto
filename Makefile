6e18d2b72e91ffa63f32212f3f26563aa268388f
manuel <manuelalonso136@gmail.com>
2019-12-15 18:28:15 +0100

    añadiendo ejecución de test de entity a makefile
    closes #50

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ install:
 	python3 setup.py install
 
 test:
-	cd bot && cd test && python3 test_app.py
+	cd bot && cd test && python3 test_app.py && python3 test_result.py
 
 ejecutar:
 	cd bot && python3 app.py
--- bot/test/test_result.py
+++ bot/test/test_result.py
@@ -19,3 +19,6 @@ class ResultTest(unittest.TestCase):
     def test_result_visit(self):
         result = Result(teamLocal="Granada", teamVisit="Valladolid", resultLocal=1, resultVisit=1)
         self.assertEqual(result.getResultVisit(), 1)
+
+if __name__ == '__main__':
+   unittest.main()
